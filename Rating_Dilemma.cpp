b1e32afdb5b1b4d5d66936a99e716c2aed8e296a
asheeshkumar12 <91361484+asheeshkumar12@users.noreply.github.com>
2022-03-26 23:16:11 +0530

    Create Rating_Dilemma.cpp
    Chef really likes to compete on Codechef, and he has gained an impressive rating of X, where X>0. There is also a parallel universe, where ratings on Codechef are negative instead. The only thing that remains unchanged in the parallel universe is Chef's love for competing on Codechef. Chef's rating on Codechef in the parallel universe is Y, where Y<0.
    
    Due to some cosmic event, the parallel universe has been destroyed, resulting in Chef forgetting both X and Y. He only remembers the sum S=X+Y. He wonders what the maximum possible product of his ratings is, given that he knows the sum of his ratings.
    
    Input Format
    The first line of input will contain an integer T — the number of test cases. The description of T test cases follows.
    The first and only line of each test case contains an integer S, the sum of Chef's ratings.
    Output Format
    For each test case, output the maximum possible product of Chef's ratings, given that he knows the sum of his ratings.
    Constraints
    1≤T≤103
    0≤S≤109
    Subtasks
    Subtask #1 (100 points): Original constraints
    
    Sample Input 1 
    2
    0
    1
    Sample Output 1 
    -1
    -2
    Explanation
    Test case 1: We have X>0 and X+Y=0. This implies that Y=−X. The product of ratings is −X^2, and the maximum possible product is −1.

---
+++ Rating_Dilemma.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define line cout<<"\n";
+#define ll int long long
+#define pu push_back
+#define ff first
+#define ss second
+void solve()
+{
+      int t;
+      cin>>t;  // test case input
+      while(t--)
+      {
+           int s;   //given sum;
+           cin>>s;
+           int a=1,b;
+           b=-a-s;
+           cout<<b<<endl;
+           
+           
+      }
+      
+}
+int main()
+{
+       solve();
+}
