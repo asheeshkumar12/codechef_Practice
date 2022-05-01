e9b6dd58cabdd02586ebee7cc179df2905d37bdd
Asheesh Kumar <91361484+asheeshkumar12@users.noreply.github.com>
2022-05-01 20:46:37 +0530

    Create Farmer League.cpp

---
+++ Farmer League.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define line cout<<"\n";
+#define ll int long long
+#define pu push_back
+#define ff first
+#define ss second
+void solve()
+{
+      int t;
+      cin>>t;
+      while(t--)
+      {
+            int a;
+            cin>>a;
+            int t=(a/2);
+            cout<<t*3<<endl;
+      }
+      
+}
+int main()
+{
+       solve();
+}
